0496ec9ed9a29f1d0e5acd95a08bd8fd4c5330ff
YestoryFornever <guosj90@163.com>
2017-03-25 14:26:34 +0800

    循环应该放在render里面

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,16 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
 import SubMessage from './component';
-var submessages = [];
-for (var i = 0; i < 10; i++) {
-	submessages.push(<SubMessage key={i}/>);
-}
+
 class Hello extends React.Component{
 	clickMe(){
 		alert('你刚才点击了我！！');
 	}
 	render(){
+		let submessages = [];
+		for (let i = 0; i < 10; i++) {
+			submessages.push(<SubMessage key={i}/>);
+		}
 		return (<div>
 			<h1 onClick={this.clickMe}>Hello</h1>
 			{submessages}
