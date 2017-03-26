e60d9e5201ce92b65db19ce0a86d84e3a0292f30
YestoryFornever <guosj90@163.com>
2017-03-26 19:06:39 +0800

    count interval

--- src/main.jsx
+++ src/main.jsx
@@ -13,66 +13,34 @@ class Hello extends React.Component{
 			selectValue:'1',
 			radioValue:'',
 			checkboxValue:[],
-			textareaValue:'textarea value...'
+			textareaValue:'textarea value...',
+			countNum:0
 		};
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleRadio = this.handleRadio.bind(this);
-		this.handleCheckbox = this.handleCheckbox.bind(this);
+		console.log('init');
 	}
-	handleSubmit(e){
-		e.preventDefault();
-		// console.log(e);
-		console.log({
-			input:this.refs['refInput'].value,
-			select:this.refs['refSelect'].value,
-			radio:this.state.radioValue,
-			checkbox:this.state.checkboxValue,
-			textarea:this.refs['refTextarea'].value,
-		});
-		this.refs['refSubmsg'].subfn();
+	componentWillMount(){
+		console.log('willmount');
 	}
-	handleRadio(e){
-		this.setState({
-			radioValue:e.target.value
-		});
+	componentDidMount(){
+		console.log('didmount');
+		this.xxx = setInterval(()=>{
+			this.setState({
+				countNum:this.state.countNum+1
+			});
+		},1000);
 	}
-	supfn(){
-		console.info('...super info');
+	componentWillUnmount(){
+		console.log('willunmount');
+		clearInterval(this.xxx);
 	}
-	handleCheckbox(e){
-		let curCheckboxValues = this.state.checkboxValue.slice();
-		if(!curCheckboxValues.includes(e.target.value)){
-			curCheckboxValues.push(e.target.value);
-		}else{
-			curCheckboxValues.splice(curCheckboxValues.indexOf(e.target.value),1);
-		}
-		this.setState({
-			checkboxValue:curCheckboxValues
-		})
+	kill(){
+		ReactDOM.unmountComponentAtNode(document.getElementById('app'));
 	}
 	render(){
 		return (<div>
-			<SubMessage ref='refSubmsg' supfn={this.supfn} messages={this.state.messages}/>
-			<form onSubmit={this.handleSubmit}>
-				新文本框：<input ref={(comp)=>{ReactDOM.findDOMNode(comp).focus()}} defaultValue='1'/>
-				<input ref='refInput' defaultValue={this.state.inputValue}/><br/>
-				<select ref='refSelect' defaultValue={this.state.selectValue}>
-					<option defaultValue='1'>one</option>
-				</select><br/>
-				{/*处理radio／checkbox最好还是用onchange*/}
-				<label htmlFor='a'>a</label>
-				<input name='abc' type='radio' onChange={this.handleRadio} value='a' />
-				<input name='abc' type='radio' onChange={this.handleRadio} value='b' />
-				<input name='abc' type='radio' onChange={this.handleRadio} value='c' />
-				<br/>
-				<label htmlFor='x'>x</label>
-				<input name='xyz' type='checkbox' onChange={this.handleCheckbox} value='x'/>
-				<input name='xyz' type='checkbox' onChange={this.handleCheckbox} value='y'/>
-				<input name='xyz' type='checkbox' onChange={this.handleCheckbox} value='z'/>
-				<br/>
-				<textarea ref='refTextarea' defaultValue={this.state.textareaValue}></textarea><br/>
-				<button type='submit'>submit</button>
-			</form>
+			{this.state.countNum}
+			<button onClick={this.kill}>test</button>
+			<SubMessage ref='refSubmsg'/>
 		</div>);
 	}
 };
