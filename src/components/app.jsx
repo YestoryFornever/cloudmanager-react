daebccb9dbfa4615e09cefa1c2c1641b7fe116d5
YestoryFornever <guosj90@163.com>
2017-04-11 22:43:40 +0800

    简写形式

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,28 @@ import AddTodo from './add-todo'
 import TodoList from './todo-list'
 import Footer from './footer'
 
-export default class App extends Component {
-	/*constructor(props){
+const App = ({ visibleTodos, visibilityFilter, onAddClick, onTodoClick, onFilterChange }) => {
+	return (
+		<div>
+			<AddTodo
+				onAddClick={onAddClick} />
+			<TodoList
+				todos={visibleTodos}
+				onTodoClick={onTodoClick}
+				/>
+			<Footer
+				filter={visibilityFilter}
+				onFilterChange={onFilterChange} />
+		</div>
+	)
+};
+export default App;
+
+/*export default class App extends Component {
+	constructor(props){
 		super();
 		console.log(props);//即根组件Provider中的store
-	}*/
+	}
 	render() {
 		// Injected by connect() call:
 		const { visibleTodos, visibilityFilter, onAddClick, onTodoClick, onFilterChange } = this.props
@@ -25,4 +42,4 @@ export default class App extends Component {
 			</div>
 		)
 	}
-}
+}*/
