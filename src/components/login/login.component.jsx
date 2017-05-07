473a46c3b1391273d77d08caa3ec20ea8cd2c7e9
YestoryFornever <guosj90@163.com>
2017-05-07 23:20:30 +0800

    登录页面 init

--- src/components/login/login.component.jsx
+++ src/components/login/login.component.jsx
@@ -1,11 +1,22 @@
+import './login.less';
 import React, { Component, PropTypes } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+// <div style={{width:'100px',height:'100px'}}></div>
 const Login = ({}) => {
 	return (
 		<div>
-			<h3>login</h3>
-			<button><Link to="/home">登录</Link></button>
+			<header>
+				<h3>login</h3>
+			</header>
+			<main>
+				<section className="login-img">
+					<img src={require("./images/webfirewall.jpg")}/>
+				</section>
+				<section className="login-dialog">
+					<button><Link to="/home">登录</Link></button>
+				</section>
+			</main>
 		</div>
 	)
 };
